4bd0d0cecd8d430af6c6920bcadca0e0b42e20fe
nighteye3 <106421478+nighteye3@users.noreply.github.com>
2022-05-28 14:24:22 +0530

    Update AnimeFlixIcon.tsx

--- src/components/AnimeFlixIcon.tsx
+++ src/components/AnimeFlixIcon.tsx
@@ -3,7 +3,7 @@ export interface AnimeFlixProps {
 }
 
 const AnimeFlixIcon: React.FC<AnimeFlixProps> = ({ className }) => (
-<img src="https://www.linkpicture.com/q/1585311326229.png" type="image"/>
+<img src="https://www.linkpicture.com/q/1585311326229.png" alt="image"/>
 );
 
 export default AnimeFlixIcon;
